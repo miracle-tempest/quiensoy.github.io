9848ee2a6832f0702f9ba8d0f7cc474db011981e
ekyl <nkdigiulio@gmail.com>
2015-05-11 19:50:06 -0600

    UPdate

--- application.js
+++ application.js
@@ -1,18 +1,19 @@
 $(document).ready(function() {
-	$('span').hasClass('1').on('click', function() {
+	$('span.1').on('click', function() {
 		alert('sadfsd');
 		$('.content').find('.1').show();
 	});
 
-	$('span').hasClass('2').on('click', function() {
+	$('span.2').on('click', function() {
 		$('.content').find('.2').show();
 	});
 
-	$('span').hasClass('3').on('click', function() {
+	$('span.3').on('click', function() {
 		$('.content').find('.3').show();
+		alert('asfdsadfsadf');
 	});
 
-	$('span').hasClass('4').on('click', function() {
+	$('span.4').on('click', function() {
 		$('.content').find('.4').show();
 	});
 
