118669342864481905771b38dedf53aeb7857ff7
ekyl <nkdigiulio@gmail.com>
2015-05-11 19:41:36 -0600

    update

--- application.js
+++ application.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
 	$('.1').on('click' 'span', function() {
+		alert('sadfsd');
 		$('.content').find('.1').show();
+	});
+
+	$('.2').on('click' 'span', function() {
+		$('.content').find('.2').show();
+	});
+
+	$('.3').on('click' 'span', function() {
+		$('.content').find('.3').show();
+	});
+
+	$('.4').on('click' 'span', function() {
+		$('.content').find('.4').show();
 	});
 
 
