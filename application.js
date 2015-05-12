fa68794bb79197027e14b4ed7910121ba86ff1f4
ekyl <nkdigiulio@gmail.com>
2015-05-11 20:01:02 -0600

    update

--- application.js
+++ application.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
 	$('.content').find('.1').slideUp(1);
 	$('span.1').on('click', function() {
-		$('.content').find('.1').slideToggle('slow');
+		$('.content').find('.1').slideToggle();
 	});
 
 	$('span.2').on('click', function() {
-		$('.content').find('.2').slideToggle('slow');
+		$('.content').find('.2').slideToggle();
 	});
 
 	$('span.3').on('click', function() {
-		$('.content').find('.3').slideToggle('slow');
+		$('.content').find('.3').slideToggle();
 	});
 
 	$('span.4').on('click', function() {
-		$('.content').find('.4').slideToggle('slow');
+		$('.content').find('.4').slideToggle();
 	});
 
 
