a58c4af37950acb55dbaab0ebcc7dd7eaa6c8e25
ekyl <nkdigiulio@gmail.com>
2015-05-11 20:35:32 -0600

    adding text

--- application.js
+++ application.js
@@ -1,26 +1,26 @@
 $(document).ready(function() {
 	$('span.1').on('click', function() {
-		$('.content').find('*').hide();
+		$('.content').children('*').hide();
 		$('.content').find('.1').show();
 	});
 
 	$('span.2').on('click', function() {
-		$('.content').find('*').hide();
+		$('.content').children('*').hide();
 		$('.content').find('.2').show();
 	});
 
 	$('span.3').on('click', function() {
-		$('.content').find('*').hide();
+		$('.content').children('*').hide();
 		$('.content').find('.3').show();
 	});
 
 	$('span.4').on('click', function() {
-		$('.content').find('*').hide();
+		$('.content').children('*').hide();
 		$('.content').find('.4').show();
 	});
 
 	$('span.5').on('click', function() {
-		$('.content').find('*').hide();
+		$('.content').children('*').hide();
 		$('.content').find('.5').show();
 	});
 
