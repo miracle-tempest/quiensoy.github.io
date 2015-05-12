e1ddd6f34adb81830704de5395002ae2030cf8b3
ekyl <nkdigiulio@gmail.com>
2015-05-11 19:52:59 -0600

    fixing animations

--- application.js
+++ application.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
 	$('span.1').on('click', function() {
-		$('.content').find('.1').slideDown();
+		$('.content').find('.1').slideToggle('slow');
 	});
 
 	$('span.2').on('click', function() {
-		$('.content').find('.2').slideDown();
+		$('.content').find('.2').slideToggle('slow');
 	});
 
 	$('span.3').on('click', function() {
-		$('.content').find('.3').slideDown();
+		$('.content').find('.3').slideToggle('slow');
 	});
 
 	$('span.4').on('click', function() {
-		$('.content').find('.4').slideDown();
+		$('.content').find('.4').slideToggle('slow');
 	});
 
 
