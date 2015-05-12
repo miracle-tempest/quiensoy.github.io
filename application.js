56125c2ffd8916254b2784f674a30f27e5e5e870
ekyl <nkdigiulio@gmail.com>
2015-05-11 21:06:29 -0600

    fixing delay by making the disappear .hide()

--- application.js
+++ application.js
@@ -1,26 +1,26 @@
 $(document).ready(function() {
 	$('span.1').on('click', function() {
-		$('.content').children('*').fadeOut();
+		$('.content').children('*').hide();
 		$('.content').find('.1').fadeIn();
 	});
 
 	$('span.2').on('click', function() {
-		$('.content').children('*').fadeOut();
+		$('.content').children('*').hide();
 		$('.content').find('.2').fadeIn();
 	});
 
 	$('span.3').on('click', function() {
-		$('.content').children('*').fadeOut();
+		$('.content').children('*').hide();
 		$('.content').find('.3').fadeIn();
 	});
 
 	$('span.4').on('click', function() {
-		$('.content').children('*').fadeOut();
+		$('.content').children('*').hide();
 		$('.content').find('.4').fadeIn();
 	});
 
 	$('span.5').on('click', function() {
-		$('.content').children('*').fadeOut();
+		$('.content').children('*').hide();
 		$('.content').find('.5').fadeIn();
 	});
 
