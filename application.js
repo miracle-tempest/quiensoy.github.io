30edafe827d39b42ebb254041e1eb1fd8e271f8b
ekyl <nkdigiulio@gmail.com>
2015-05-11 20:05:40 -0600

    update

--- application.js
+++ application.js
@@ -1,19 +1,27 @@
 $(document).ready(function() {
-	$('.content').find('.1').slideUp(1);
 	$('span.1').on('click', function() {
-		$('.content').find('.1').slideToggle();
+		$('.content').find('*').hide();
+		$('.content').find('.1').show();
 	});
 
 	$('span.2').on('click', function() {
-		$('.content').find('.2').slideToggle();
+		$('.content').find('*').hide();
+		$('.content').find('.2').show();
 	});
 
 	$('span.3').on('click', function() {
-		$('.content').find('.3').slideToggle();
+		$('.content').find('*').hide();
+		$('.content').find('.3').show();
 	});
 
 	$('span.4').on('click', function() {
-		$('.content').find('.4').slideToggle();
+		$('.content').find('*').hide();
+		$('.content').find('.4').show();
+	});
+
+	$('span.5').on('click', function() {
+		$('.content').find('*').hide();
+		$('.content').find('.5').show();
 	});
 
 
